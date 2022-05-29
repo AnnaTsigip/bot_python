905c842dc7b51c9ad95ba4a8a07641e753445048
Anna <anna_tsigip@mail.ru>
2022-05-29 19:50:13 +0300

    bot

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ import telebot
 bot = telebot.TeleBot('5198683969:AAF_PzZ82SizhqndIrhKv6gDCIQzXgyBklc') # привязка к боту
 
 # отслеживание команд
-@bot.message_handler(commands_types=['text'])
-def start(message):
+@bot.message_handler(commands=['start'])
+def start(message, res=False):
     #bot.send_message(message.chat.id, 'Привет')#'<b>Hello</b>', parse_mode='html')
     if message.text == "Привет":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
