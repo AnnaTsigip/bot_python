cf80ae3195162d91cb62c7f8ba676fef98dfde00
Anna <anna_tsigip@mail.ru>
2022-05-31 21:20:15 +0300

    candies bot

--- bot2.py
+++ bot2.py
@@ -2,13 +2,16 @@ import telebot
 
 # Создаем экземпляр бота
 bot = telebot.TeleBot('5538048280:AAEXviBNV6jBOhZBgOO6uzQu0nJTO0TRleY')
+
 # Функция, обрабатывающая команду /start
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
     bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+
 # Получение сообщений от юзера
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+
 # Запускаем бота
 bot.polling(none_stop=True, interval=0)
